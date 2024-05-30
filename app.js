2f324fd5f864dcee78367ceb61354f0385bed993
smefti70 <csembstu19009@gmail.com>
2024-05-30 16:08:20 +0600

    All necessary files has been  commited.

---
+++ app.js
@@ -0,0 +1,90 @@
+document.getElementById('basic-btn').addEventListener('click', function() {
+    this.disabled = true;
+    document.getElementById('basic-btn').style.backgroundColor = '#FFF';
+    document.getElementById('basic-btn').style.color = '#281E29';
+
+    let Rooms = document.getElementById('basic-plan-room-no').innerText;
+    Rooms = parseInt(Rooms);
+    console.log(Rooms)
+
+    let newDiv = document.createElement('div');
+    newDiv.id = 'basic-greeting';
+    newDiv.innerHTML = 
+    `Thank you for choosing <span id="basic-room-no">${Rooms}</span> room.`
+    console.log(newDiv)
+    document.getElementById('basic-plan-container').appendChild(newDiv);
+});
+
+
+document.getElementById('basic-minus').addEventListener('click',function(){
+    let Price = document.getElementById('basic-plan').innerText;
+    let Rooms = document.getElementById('basic-plan-room-no').innerText;
+    Price = parseInt(Price);
+    Rooms = parseInt(Rooms);
+
+    if(Price>199)
+        {
+            Price-=199;
+            Rooms-=1;
+        }
+        
+    document.getElementById('basic-plan').innerText = Price;
+    document.getElementById('basic-plan-room-no').innerText = Rooms;
+})
+
+document.getElementById('basic-plus').addEventListener('click',function(){
+    let Price = document.getElementById('basic-plan').innerText;
+    let Rooms = document.getElementById('basic-plan-room-no').innerText;
+    Price = parseInt(Price);
+    Rooms = parseInt(Rooms);
+    Price+=199;
+    Rooms+=1;
+    document.getElementById('basic-plan').innerText = Price;
+    document.getElementById('basic-plan-room-no').innerText = Rooms;
+})
+
+document.getElementById('pro-minus').addEventListener('click',function(){
+    let Price = document.getElementById('pro-plan').innerText;
+    let Rooms = document.getElementById('pro-plan-room-no').innerText;
+    Price = parseInt(Price);
+    Rooms = parseInt(Rooms);
+
+    if(Price>249)
+        {
+            Price-=249;
+            Rooms-=1;
+        }
+        
+    document.getElementById('pro-plan').innerText = Price;
+    document.getElementById('pro-plan-room-no').innerText = Rooms;
+})
+
+document.getElementById('pro-plus').addEventListener('click',function(){
+    let Price = document.getElementById('pro-plan').innerText;
+    let Rooms = document.getElementById('pro-plan-room-no').innerText;
+    Price = parseInt(Price);
+    Rooms = parseInt(Rooms);
+    Price+=249;
+    Rooms+=1;
+    document.getElementById('pro-plan').innerText = Price;
+    document.getElementById('pro-plan-room-no').innerText = Rooms;
+})
+
+
+
+document.getElementById('pro-btn').addEventListener('click',function(){
+    this.disabled = true;
+    document.getElementById('pro-btn').style.backgroundColor = '#FFF';
+    document.getElementById('pro-btn').style.color = '#281E29';
+    
+    let Rooms = document.getElementById('pro-plan-room-no').innerText;
+    Rooms = parseInt(Rooms);
+    console.log(Rooms)
+
+    let newDiv = document.createElement('div');
+    newDiv.id = 'pro-greeting';
+    newDiv.innerHTML = 
+    `Thank you for choosing <span id="pro-room-no">${Rooms}</span> room.`
+    console.log(newDiv)
+    document.getElementById('pro-plan-container').appendChild(newDiv);
+})
